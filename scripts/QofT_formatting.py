fa9584c4f664f75df04784e69776a0696dfd0fe4
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2023-12-24 18:42:41 +0600

    ref/ script for QofT

--- scripts/QofT_formatting.py
+++ scripts/QofT_formatting.py
@@ -11,33 +11,19 @@ for i, line in enumerate(lines):
     if not cleaned_line:
         empty_lines.append(i)
 
-print('empty_lines_indices: ', empty_lines)
-print('len of processed lines: ', len(processed_lines))
-input()
-
 header = processed_lines[0]
-print(header)
-print('len of processed lines: ', len(processed_lines))
-input()
 
 data_lines = []
 for i, empty_line in enumerate(empty_lines):
     if i + 1 < len(empty_lines):
-        print(i, empty_lines[i], empty_lines[i+1])
         mm = empty_lines[i] + 1
         nn = empty_lines[i+1]
-        print(mm, ':', nn)
-        print('len of sliced processed line: ', len(processed_lines[mm:nn]))
-        input()
         data_line = ''.join(processed_lines[mm:nn])
         data_lines.append(data_line)
     else:
         break
 
-print(len(data_lines))
-
-# Write to the output file
 with open('data/QofT_formatted.dat', 'w') as file:
-    file.write(header + '\n')  # Write the dimension line
+    file.write(header + '\n') 
     for data_line in data_lines:    
-        file.write(data_line + '\n')  # Write the processed data line
+        file.write(data_line + '\n')
