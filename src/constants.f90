5043d77fc1d43d4dc8c027025ba6f0f7619d78cb
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2024-01-22 00:44:17 +0600

    introduce constants module

--- src/constants.f90
+++ src/constants.f90
@@ -7,4 +7,12 @@ module constants
     real, parameter :: BOL = 1.3806503e-23
     real, parameter :: C1 = 2 * PI * PLANCK * (SPL**2)
     real, parameter :: C2 = (PLANCK * SPL / BOL) * 100 ! 1.438769 cm * K
+    real, parameter :: stPressure = 1. ! in atmospheres
+    real, parameter :: stTemperature = 273.15 ! in K
+    real, parameter :: LOSCHMIDT = 2.6867811e25
+    real, parameter :: AVOGADRO = 6.02214076e23
+    real, parameter :: gasCONST = AVOGADRO * BOL
+
+    real, parameter :: refTemperature = 296. ! in K
+
 end module constants
