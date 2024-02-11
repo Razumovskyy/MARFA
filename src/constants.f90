f4f7af2142ab85792a273202da32059cb87c7d84
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2024-02-11 21:01:32 +0400

    set all constants to cgs

--- src/constants.f90
+++ src/constants.f90
@@ -1,12 +1,12 @@
 module constants
     implicit none
     real, parameter :: PI = 3.1415926
-    real, parameter :: PLANCK = 6.626070e-34
+    real, parameter :: PLANCK = 6.626070e-27
     real, parameter :: redPLANCK = PLANCK / (2 * PI)
-    real, parameter :: SPL = 2.99792458e8
-    real, parameter :: BOL = 1.3806503e-23
+    real, parameter :: SPL = 2.99792458e10
+    real, parameter :: BOL = 1.3806503e-16
     real, parameter :: C1 = 2 * PI * PLANCK * (SPL**2)
-    real, parameter :: C2 = (PLANCK * SPL / BOL) * 100 ! 1.438769 cm * K
+    real, parameter :: C2 = (PLANCK * SPL / BOL) ! 1.438769 cm * K
     real, parameter :: stPressure = 1. ! in atmospheres
     real, parameter :: stTemperature = 273.15 ! in K
     real, parameter :: LOSCHMIDT = 2.6867811e25
