80c9ea503887763b448461ade439f276186e69eb
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2024-03-06 00:52:26 +0400

    remove old files during Camel refactor

--- src/shape_functions.f90
+++
@@ -1,8 +0,0 @@
-module shape_functions
-    implicit none
-    abstract interface
-        real function shape_func(x)
-            real, intent(in) :: x
-        end function shape_func
-    end interface
-end module shape_functions
