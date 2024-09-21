17f1702d8d13a0d5aacb6530808768287b992021
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2024-09-21 15:08:01 +0400

    ref: legacy chit

--- scripts/chit.f90
+++ scripts/chit.f90
@@ -1,48 +1,53 @@
-PROGRAM DIR_CHIT
+! legacy fortran script for plotting and data processing !
+program chit
 implicit none
-integer, PARAMETER :: NT=20481
+integer, parameter :: NT=20481
 integer :: I, NCASE
 integer :: JN
 integer :: M, N, NZ
 real :: H
-CHARACTER MET*3,DIR_NAME*4
+character MET*3, DIR_NAME*4
 real ::  RK(NT)
 real :: V, VV
 
-!!! INPUT EXTENSION !!!
-MET='CO2'
+MET='CO2' ! input extension
 
-H=10.0/(NT-1)
+H = 10.0 / (NT-1)
 ! -------------------------------!
-DO NCASE=1,5
-WRITE(*,*)'  V   J  (0.0 or 0 => STOP) '
-READ(*,*)V,JN
-DIR_NAME='___.'
-IF(V==0.0.OR.JN==0)STOP 
-IF(JN<10)THEN
-WRITE(DIR_NAME(1:1),'(I1)')JN   
-ELSE
-     IF(JN<100)THEN
-WRITE(DIR_NAME(1:2),'(I2)')JN
-	 ELSE
-WRITE(DIR_NAME(1:3),'(I3)')JN
-	 END IF
-END IF
-NZ = (V+1.)/10.
-OPEN(491,ACCESS='DIRECT', FORM='UNFORMATTED',	&
- RECL = NT*4, FILE='./output/PT_CALC/'//DIR_NAME//MET) ! NT*4 for UNIX
-!WRITE(*,*) './PT_CALC/'//DIR_NAME//MET
-!PAUSE 
-READ(491,REC=NZ)RK
-CLOSE(491)
-OPEN(88,FILE='SPECTR')
-	N=NT ! 10000
-	M=1
-	WRITE(88,*)M,N
-             DO I=1,N 
-VV=V+H*(I-1)
-WRITE(88,*)VV,ALOG10(RK(I))
-             END DO
-CLOSE(88)
-END DO
-END PROGRAM DIR_CHIT
+do NCASE = 1, 5
+	write(*,*)'  V   J  (0.0 or 0 => STOP) '
+	read(*,*) V, JN
+	
+	DIR_NAME='___.'
+	
+	if (V==0.0 .or. JN==0) STOP 
+	
+	if (JN < 10) then
+	write(DIR_NAME(1:1),'(I1)') JN   
+	else
+		if (JN < 100) then
+			write(DIR_NAME(1:2), '(I2)') JN
+		else
+			write(DIR_NAME(1:3), '(I3)') JN
+		end if
+	end if
+
+	NZ = (V+1.)/10.
+	
+	open(491, access='DIRECT', form='UNFORMATTED',	&
+ 		recl = NT*4, file='./output/PT_CALC/'//DIR_NAME//MET) ! NT*4 for UNIX
+
+	read(491, rec=NZ) RK ! reads only one record with deltaWV=10 cm-1 interval data
+	close(491)
+	
+	open(88, file='SPECTR')
+	N = NT ! 10000
+	M = 1
+	write(88,*) M, N
+            do I = 1, N 		
+				VV = V + H*(I-1)
+				write(88,*) VV, ALOG10(RK(I))
+			end do
+			close(88)
+		end do
+end program chit
