9262981e08b5b672c836e4667dbaad48fdeb431c
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2024-12-14 07:05:49 +0400

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Find and delete all .mod files in the src directory
+# Removes all dependencies (new build is needed after that)
+# Find and delete all .par files in the root directory and its subdirectories
+# Find and delete all .dat fiels in the root directory only
+
 CURRENT_DIR=$(basename "$PWD")
 
 if [ "$CURRENT_DIR" != "MARFA" ]; then
@@ -7,10 +12,8 @@ if [ "$CURRENT_DIR" != "MARFA" ]; then
   exit 1
 fi
 
-# Find and delete all .par files in the root directory and its subdirectories
-# Find and delete all .dat fiels in the root directory only
+find ./src -type f -name "*.mod" -exec rm -f {} +
+find ./build -mindepth 1 -exec rm -rf {} +
 find . -type f -name "*.par" -exec rm -f {} +
 find . -maxdepth 1 -type f -name "*.dat" -exec rm -f {} +
 
-echo "All .par files in the root directory and its subdirectories have been deleted."
-echo "All .dat files in the root directory are deleted"
