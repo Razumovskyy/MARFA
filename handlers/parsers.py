9d37dce243bae4803be9f446a1d2e4692d7b1563
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2025-02-18 18:43:25 +0400

    ref: handling invalid level error

--- handlers/parsers.py
+++ handlers/parsers.py
@@ -6,6 +6,7 @@ Description:
     This module is a part of the MARFA-webapp project.
 """
 import os
+import sys
 from pathlib import Path
 
 import numpy as np
@@ -40,23 +41,27 @@ def base_parser(pttable_file: Path, v1: float, v2: float) -> tuple[list[np.float
     vw_data = []
     absorption_data = []
     record_number = start_record_number
-    with open(pttable_file, 'rb') as f:
-        for j in range(1, num_records + 1):
-            seek_position = (record_number - 1) * RECORD_SIZE
-            if seek_position >= os.path.getsize(pttable_file):
-                raise IndexError(f"Record number {record_number} exceeds a file size.")
-            f.seek(seek_position)
-            binary_abs_data = f.read(RECORD_SIZE)  # reads absorption data from one record
+    try:
+        with open(pttable_file, 'rb') as f:
+            for j in range(1, num_records + 1):
+                seek_position = (record_number - 1) * RECORD_SIZE
+                if seek_position >= os.path.getsize(pttable_file):
+                    raise IndexError(f"Record number {record_number} exceeds a file size.")
+                f.seek(seek_position)
+                binary_abs_data = f.read(RECORD_SIZE)  # reads absorption data from one record
 
-            abs_data = np.frombuffer(binary_abs_data, dtype=np.float32)
+                abs_data = np.frombuffer(binary_abs_data, dtype=np.float32)
 
-            in_record_start_wv = RECORD_WV_SPAN * record_number
-            for i in range(POINTS_PER_RECORD):
-                vw = in_record_start_wv + i * step
-                vw_data.append(np.float32(vw))
-                absorption_data.append(np.float32(abs_data[i]))
+                in_record_start_wv = RECORD_WV_SPAN * record_number
+                for i in range(POINTS_PER_RECORD):
+                    vw = in_record_start_wv + i * step
+                    vw_data.append(np.float32(vw))
+                    absorption_data.append(np.float32(abs_data[i]))
 
-            record_number = start_record_number + j
+                record_number = start_record_number + j
+    except FileNotFoundError:
+        print(f"Error: invalid number of atmospheric level")
+        sys.exit()
     return vw_data, absorption_data
 
 
